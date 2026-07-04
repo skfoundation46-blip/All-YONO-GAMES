7872f2694f72696d18a6f15d9c6c4250ff469d20
skfoundation46-blip <skfoundation46@gmail.com>
2026-07-04 13:34:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const search = document.getElementById("search");
+const container = document.getElementById("games");
 
+search.addEventListener("keyup", function () {
+  const value = search.value.toLowerCase();
+
+  const cards = document.querySelectorAll(".game-card");
+
+  cards.forEach(card => {
+    const name = card.querySelector("h3").innerText.toLowerCase();
+
+    if (name.includes(value)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
