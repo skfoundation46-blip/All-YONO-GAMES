a3a46a797692583c72290e68de7dffa9644e5955
skfoundation46-blip <skfoundation46@gmail.com>
2026-07-06 10:37:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -83,3 +83,38 @@ this.src="images/default.png";
 };
 
 });
+// ===== AUTO BANNER SLIDER =====
+
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+function showSlide(index){
+
+slides.forEach((slide)=>{
+
+slide.classList.remove("active");
+
+});
+
+slides[index].classList.add("active");
+
+}
+
+function nextSlide(){
+
+currentSlide++;
+
+if(currentSlide>=slides.length){
+
+currentSlide=0;
+
+}
+
+showSlide(currentSlide);
+
+}
+
+showSlide(currentSlide);
+
+setInterval(nextSlide,3000);
