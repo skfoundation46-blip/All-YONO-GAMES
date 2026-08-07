021846fd71dfe9758d33750bd43c9fead81cbdfc
skfoundation46-blip <skfoundation46@gmail.com>
2026-08-07 17:02:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,446 +1,496 @@
 const games = [
-
-{
-name:"Max Rummy",
-image:"gamerummy.png",
-bonus:"Welcome Bonus ₹500",
-rating:"4.9",
-link:"https://www.maxrummy123.com/?code=QUMLDFCQZCC&t=1784950478"
-},
-    <a href="${game.page}" class="download-btn">
-Download
-</a>
-{
-name:"Go Spin",
-image:"gospin.png",
-bonus:"Welcome Bonus ₹450",
-rating:"4.8",
-link:"https://www.gospin.bet/?code=498S6EMARN5&t=1783045757"
-},
-
-{
-name:"Hindi 777",
-image:"Hindi777.png",
-bonus:"Welcome Bonus ₹500",
-rating:"4.9",
-link:"https://www.hindi777agent5.com/?code=7LFCL9PJ516&t=1783045793"
-},
-
-{
-name:"Hi Rummy",
-image:"HiRummy.png",
-bonus:"Welcome Bonus ₹350",
-rating:"4.8",
-link:"https://joinhirummy.cc/?code=RX3HPNUFL1W&t=1783045895"
-},
-
-{
-name:"Ind Club",
-image:"indclub.png",
-bonus:"Welcome Bonus ₹400",
-rating:"4.7",
-link:"https://indclub36.com/?code=34UGAHJAEGU&t=1783045944"
-},
-
-{
-name:"Ind Slots",
-image:"indslots.png",
-bonus:"Welcome Bonus ₹450",
-rating:"4.8",
-link:"https://www.indslotsgame.com/?code=T2Q18VEZGLG&t=1783045984"
-},
-
-{
-name:"MQM Bet",
-image:"mqmbet.png",
-bonus:"Welcome Bonus ₹500",
-rating:"4.8",
-link:"https://6mqmshare.vip?pid=337905532&channel=2030000&pkg=com.mqmbet3001.app"
-},
-
-{
-name:"INR Rummy",
-image:"inrrummy.png",
-bonus:"Welcome Bonus ₹350",
-rating:"4.8",
-link:"https://inrrummysvip2.net/?code=JMQKAVX7KWE&t=1783046109"
-},
-
-{
-name:"Jaiho Arcade",
-image:"jaihoarcade.png",
-bonus:"Welcome Bonus ₹450",
-rating:"4.9",
-link:"https://www.jaihoarcade34.com/?code=AZDCZKW7BNV&t=1783046145"
-},
-
-{
-name:"Jaiho Slots",
-image:"jaihoslots.png",
-bonus:"Welcome Bonus ₹400",
-rating:"4.8",
-link:"https://www.jaihoslots18.com/?code=EGP7RY4XTDY&t=1783046174"
-},
-
-{
-name:"Jaiho Spin",
-image:"jaihospin.png",
-bonus:"Welcome Bonus ₹350",
-rating:"4.8",
-link:"https://1jaihospinslot.com/?code=7TA6DT11LF3&t=1783046216"
-},
-
-{
-name:"Jaiho Win",
-image:"jaihowin.png",
-bonus:"Welcome Bonus ₹500",
-rating:"4.9",
-link:"https://www.jaihowin16.com/?code=1XN5PLDRBGJ&t=1783046440"
-},
-
-{
-name:"Jaiho 777",
-image:"jaiho777.png",
-bonus:"Welcome Bonus ₹450",
-rating:"4.9",
-link:"https://jaiho77792.com/?code=GC2PL1RSXZD&t=1783046484"
-},
-
-{
-name:"91 Jaiho",
-image:"91jaiho.png",
-bonus:"Welcome Bonus ₹300",
-rating:"4.8",
-link:"https://91jaihoapp.com/?code=C422BB6Y941&t=1783046532"
-},
-
-{
-name:"Joy Rummy",
-image:"joyrummy.png",
-bonus:"Welcome Bonus ₹400",
-rating:"4.8",
-link:"https://www.joyrummylux.com/?code=J5KY29TE89Q&t=1783046573"
-},
-
-{
-name:"Love Rummy",
-image:"loverummy.png",
-bonus:"Welcome Bonus ₹350",
-rating:"4.8",
-link:"https://www.loverummy1.com/?code=AFCJ9HGB44A&t=1783046624"
-},
-
-{
-name:"Maha Games",
-image:"mahagames.png",
-bonus:"Welcome Bonus ₹500",
-rating:"4.9",
-link:"https://on-mahagames.com/?code=J24LM8HUX75&t=1783046672"
-},
-
-{
-name:"MBM Bet",
-image:"mbmbet.png",
-bonus:"Welcome Bonus ₹450",
-rating:"4.8",
-link:"https://www.mbmbet28.com/?code=UPH8J9BHY9Q&t=1783046724"
-},
-
-{
-name:"789 Jackpots",
-image:"789jackpots.png",
-bonus:"Welcome Bonus ₹400",
-rating:"4.8",
-link:"https://join789jackpots1.com/?code=J7ZRKV1ELHS&t=1783046769"
-},
-
-{
-name:"777 OKOK",
-image:"777okok.png",
-bonus:"Welcome Bonus ₹350",
-rating:"4.8",
-link:"https://www.777okok.com/?code=H53V3D3M674&t=1783046815"
-},
-
-{
-name:"567 Slots",
-image:"567slots.png",
-bonus:"Welcome Bonus ₹450",
-rating:"4.9",
-link:"https://join567slots1.cc/?code=4NYYNGE217D&t=1783046875"
-},
-
-{
-name:"YONO 777 Hub",
-image:"yono777hub.png",
-bonus:"Welcome Bonus ₹500",
-rating:"4.9",
-link:"https://yono777hub.online/?code=F9MJTEWN4UX&t=1783047076"
-},
-
-{
-name:"YONO VIP",
-image:"yonovip.png",
-bonus:"Welcome Bonus ₹400",
-rating:"4.8",
-link:"https://uonovipfast.com/?code=3CRW27JYQQ9&t=1783047147"
-},
-
-{
-name:"YONO Slots",
-image:"yonoslots.png",
-bonus:"Welcome Bonus ₹450",
-rating:"4.8",
-link:"https://www.uonoslots.com/?code=59YKJDMVEZG&t=1783047209"
-},
-
-{
-name:"YONO Rummy",
-image:"yonorummy.png",
-bonus:"Welcome Bonus ₹500",
-rating:"4.9",
-link:"https://yonorummy047.com/?code=VIPYHS8XTPY&t=1783047280"
-},
-
-{
-name:"YONO Games",
-image:"yonogames.png",
-bonus:"Welcome Bonus ₹400",
-rating:"4.8",
-link:"https://youonogamespromocode.com/?code=TC4QC28JKLM&t=1783047332"
-},
-
-{
-name:"YONO Official",
-image:"yonoofficial.png",
-bonus:"Welcome Bonus ₹450",
-rating:"4.9",
-link:"https://yonoofficial3.com/?code=GTSRBZZ32Z9&t=1783047379"
-},
-
-{
-name:"Yes Spin",
-image:"yesspin.png",
-bonus:"Welcome Bonus ₹350",
-rating:"4.8",
-link:"https://www.yesspinnow.com/?code=47TP7UQQ1PZ&t=1783047472"
-},
-
-{
-name:"Top Rummy",
-image:"toprummy.png",
-bonus:"Welcome Bonus ₹400",
-rating:"4.8",
-link:"https://www.toprummy.cc/?code=7K9LY1QFUJ5&t=1783047569"
-},
-
-{
-name:"Spin101",
-image:"spin101.png",
-bonus:"Welcome Bonus ₹500",
-rating:"4.9",
-link:"https://spin101-c.net/?code=3X2GPDXYBEN&t=1783047712"
-},
-
-{
-name:"Spin777",
-image:"spin777.png",
-bonus:"Welcome Bonus ₹450",
-rating:"4.8",
-link:"https://spin777dd.com/?code=7V9SH1ZGF27&t=1783047786"
-},
-
-{
-name:"Spin Winner",
-image:"spinwinner.png",
-bonus:"Welcome Bonus ₹400",
-rating:"4.9",
-link:"https://spinwinner-g.com/?code=SDN6ZMNDX1U&t=1783047844"
-},
-
-{
-name:"Safe Earn",
-image:"safeearn.png",
-bonus:"Welcome Bonus ₹350",
-rating:"4.7",
-link:"https://bkfadsegtgs.safelyearnmillionsbysharingonepersonaqfxzqyj8.com/?code=ADEV1QHDXSF&t=1783047900"
-},
-
-{
-name:"Slots Spin",
-image:"slotsspin.png",
-bonus:"Welcome Bonus ₹450",
-rating:"4.8",
-link:"https://slotsspinl.com/?code=XJBM7Y8QQQH&t=1783047986"
-},
-
-{
-name:"Share477",
-image:"share477.png",
-bonus:"Welcome Bonus ₹400",
-rating:"4.8",
-link:"https://share477.com/?code=YAZKHB26CGT&t=1783048024"
-},
-
-{
-name:"Rumble Rummy",
-image:"rumblerummy.png",
-bonus:"Welcome Bonus ₹500",
-rating:"4.9",
-link:"https://www.rumblerummy333.net/?code=UC0SU9FD5QX&t=1783048070"
-},
-
-{
-name:"OK Rummy",
-image:"okrummy.png",
-bonus:"Welcome Bonus ₹350",
-rating:"4.8",
-link:"https://www.okrummy41.com/?code=H2GU19KMB2W&t=1783048126"
-},
-
-{
-name:"Rummy 77",
-image:"rummy77.png",
-bonus:"Welcome Bonus ₹400",
-rating:"4.8",
-link:"https://rummy77bonus.net/?code=F3VJQX8LLY7&t=1783048174"
-},
-
-{
-name:"Rummy 888 VIP",
-image:"rummy888vip.png",
-bonus:"Welcome Bonus ₹500",
-rating:"4.9",
-link:"https://rummy888vip38.com/?code=TPUQEBHBDCU&t=1783048215"
-},
-
-{
-name:"Rum Ludo",
-image:"rumludo.png",
-bonus:"Welcome Bonus ₹350",
-rating:"4.8",
-link:"https://rummludo.com/?code=UWPWVG4MR7Y&t=1783048276"
-},
-
-{
-name:"Rummy 91",
-image:"rummy91.png",
-bonus:"Welcome Bonus ₹450",
-rating:"4.8",
-link:"https://www.rummy91r.bet/?code=CEELB9T658N&t=1783048317"
-},
-
-{
-name:"Saga Slots",
-image:"sagaslots.png",
-bonus:"Welcome Bonus ₹400",
-rating:"4.8",
-link:"https://www.sagaslots5.com/?code=41LV9QCX16B&t=1783048379"
-},
-
-{
-name:"Teen Patti",
-image:"teenpatti.png",
-bonus:"Welcome Bonus ₹350",
-rating:"4.8",
-link:"https://www.earntp.com/m/14043"
-},
-
-{
-name:"Spin Gold",
-image:"spingold.png",
-bonus:"Welcome Bonus ₹450",
-rating:"4.9",
-link:"https://spingold4.com/?code=HLTSA9JQW5H&t=1783048464"
-},
-
-{
-name:"77 Game",
-image:"77game.png",
-bonus:"Welcome Bonus ₹500",
-rating:"4.8",
-link:"https://77game.plus/?code=QGTT8ZS"
-},
-
-{
-name:"Good Slots",
-image:"goodslots.png",
-bonus:"Welcome Bonus ₹400",
-rating:"4.8",
-link:"https://4sharegoodslots.vip?pid=531453132&channel=2030111&pkg=com.goodslots111.com"
-},
-
-{
-name:"Diwa 777",
-image:"diwaslots.png",
-bonus:"Welcome Bonus ₹450",
-rating:"4.8",
-link:"https://sharediwa7773.top?pid=518390785&channel=2010002&pkg=com.diwa777wh.com"
-},
-
-{
-name:"Diwa Slots",
-image:"diwaslots.png",
-bonus:"Welcome Bonus ₹350",
-rating:"4.7",
-link:"https://sharediwaslots3.vip?pid=450275843&channel=2030101&pkg=com.diwaslot101.app"
-}
-
+  {
+    name: "Game Rummy",
+    image: "gamerummy.png",
+    bonus: "Welcome Bonus ₹300",
+    rating: "4.9",
+    downloads: "10M+",
+    trusted: true,
+    link: "games/gamerummy.html"
+  },
+  {
+    name: "Go Spin",
+    image: "gospin.png",
+    bonus: "Welcome Bonus ₹400",
+    rating: "4.8",
+    downloads: "8M+",
+    trusted: true,
+    link: "games/gospin.html"
+  },
+  {
+    name: "Hindi777",
+    image: "Hindi777.png",
+    bonus: "Welcome Bonus ₹500",
+    rating: "4.9",
+    downloads: "12M+",
+    trusted: true,
+    link: "games/hindi777.html"
+  },
+  {
+    name: "Hi Rummy",
+    image: "HiRummy.png",
+    bonus: "Welcome Bonus ₹500",
+    rating: "4.9",
+    downloads: "9M+",
+    trusted: true,
+    link: "games/hirummy.html"
+  },
+  {
+    name: "Ind Club",
+    image: "indclub.png",
+    bonus: "Welcome Bonus ₹250",
+    rating: "4.8",
+    downloads: "7M+",
+    trusted: true,
+    link: "games/indclub.html"
+  },
+  {
+    name: "Ind Slots",
+    image: "indslots.png",
+    bonus: "Welcome Bonus ₹400",
+    rating: "4.8",
+    downloads: "6M+",
+    trusted: true,
+    link: "games/indslots.html"
+  },
+  {
+    name: "MQM Bet",
+    image: "mqmbet.png",
+    bonus: "Welcome Bonus ₹500",
+    rating: "4.7",
+    downloads: "5M+",
+    trusted: true,
+    link: "games/mqmbet.html"
+  },
+  {
+    name: "INR Rummy",
+    image: "inrrummy.png",
+    bonus: "Welcome Bonus ₹500",
+    rating: "4.9",
+    downloads: "11M+",
+    trusted: true,
+    link: "games/inrrummy.html"
+  },
+  {
+    name: "Jaiho Arcade",
+    image: "jaihoarcade.png",
+    bonus: "Welcome Bonus ₹350",
+    rating: "4.8",
+    downloads: "7M+",
+    trusted: true,
+    link: "games/jaihoarcade.html"
+  },
+  {
+    name: "Jaiho Slots",
+    image: "jaihoslots.png",
+    bonus: "Welcome Bonus ₹450",
+    rating: "4.8",
+    downloads: "6M+",
+    trusted: true,
+    link: "games/jaihoslots.html"
+  },
+      {
+    name: "Jaiho Spin",
+    image: "jaihospin.png",
+    bonus: "Welcome Bonus ₹400",
+    rating: "4.8",
+    downloads: "6M+",
+    trusted: true,
+    link: "games/jaihospin.html"
+  },
+  {
+    name: "Jaiho Win",
+    image: "jaihowin.png",
+    bonus: "Welcome Bonus ₹500",
+    rating: "4.9",
+    downloads: "8M+",
+    trusted: true,
+    link: "games/jaihowin.html"
+  },
+  {
+    name: "Jaiho777",
+    image: "jaiho777.png",
+    bonus: "Welcome Bonus ₹300",
+    rating: "4.8",
+    downloads: "5M+",
+    trusted: true,
+    link: "games/jaiho777.html"
+  },
+  {
+    name: "91 Jaiho",
+    image: "91jaiho.png",
+    bonus: "Welcome Bonus ₹350",
+    rating: "4.8",
+    downloads: "5M+",
+    trusted: true,
+    link: "games/91jaiho.html"
+  },
+  {
+    name: "Joy Rummy",
+    image: "joyrummy.png",
+    bonus: "Welcome Bonus ₹400",
+    rating: "4.8",
+    downloads: "6M+",
+    trusted: true,
+    link: "games/joyrummy.html"
+  },
+  {
+    name: "Love Rummy",
+    image: "loverummy.png",
+    bonus: "Welcome Bonus ₹300",
+    rating: "4.7",
+    downloads: "4M+",
+    trusted: true,
+    link: "games/loverummy.html"
+  },
+  {
+    name: "Maha Games",
+    image: "mahagames.png",
+    bonus: "Welcome Bonus ₹500",
+    rating: "4.9",
+    downloads: "7M+",
+    trusted: true,
+    link: "games/mahagames.html"
+  },
+  {
+    name: "MBM Bet",
+    image: "mbmbet.png",
+    bonus: "Welcome Bonus ₹450",
+    rating: "4.8",
+    downloads: "5M+",
+    trusted: true,
+    link: "games/mbmbet.html"
+  },
+  {
+    name: "789 Jackpots",
+    image: "789jackpots.png",
+    bonus: "Welcome Bonus ₹500",
+    rating: "4.9",
+    downloads: "8M+",
+    trusted: true,
+    link: "games/789jackpots.html"
+  },
+  {
+    name: "777 OKOK",
+    image: "777okok.png",
+    bonus: "Welcome Bonus ₹350",
+    rating: "4.8",
+    downloads: "6M+",
+    trusted: true,
+    link: "games/777okok.html"
+  },
+      {
+    name: "567 Slots",
+    image: "567slots.png",
+    bonus: "Welcome Bonus ₹400",
+    rating: "4.8",
+    downloads: "6M+",
+    trusted: true,
+    link: "games/567slots.html"
+  },
+  {
+    name: "YONO777 Hub",
+    image: "yono777hub.png",
+    bonus: "Welcome Bonus ₹500",
+    rating: "4.9",
+    downloads: "9M+",
+    trusted: true,
+    link: "games/yono777hub.html"
+  },
+  {
+    name: "YONO VIP",
+    image: "yonovip.png",
+    bonus: "Welcome Bonus ₹450",
+    rating: "4.8",
+    downloads: "7M+",
+    trusted: true,
+    link: "games/yonovip.html"
+  },
+  {
+    name: "YONO Slots",
+    image: "yonoslots.png",
+    bonus: "Welcome Bonus ₹350",
+    rating: "4.8",
+    downloads: "6M+",
+    trusted: true,
+    link: "games/yonoslots.html"
+  },
+  {
+    name: "YONO Rummy",
+    image: "yonorummy.png",
+    bonus: "Welcome Bonus ₹500",
+    rating: "4.9",
+    downloads: "10M+",
+    trusted: true,
+    link: "games/yonorummy.html"
+  },
+  {
+    name: "YONO Games",
+    image: "yonogames.png",
+    bonus: "Welcome Bonus ₹300",
+    rating: "4.8",
+    downloads: "5M+",
+    trusted: true,
+    link: "games/yonogames.html"
+  },
+  {
+    name: "YONO Official",
+    image: "yonoofficial.png",
+    bonus: "Welcome Bonus ₹400",
+    rating: "4.8",
+    downloads: "6M+",
+    trusted: true,
+    link: "games/yonoofficial.html"
+  },
+  {
+    name: "Yes Spin",
+    image: "yesspin.png",
+    bonus: "Welcome Bonus ₹350",
+    rating: "4.8",
+    downloads: "5M+",
+    trusted: true,
+    link: "games/yesspin.html"
+  },
+  {
+    name: "Top Rummy",
+    image: "toprummy.png",
+    bonus: "Welcome Bonus ₹500",
+    rating: "4.9",
+    downloads: "9M+",
+    trusted: true,
+    link: "games/toprummy.html"
+  },
+  {
+    name: "Spin101",
+    image: "spin101.png",
+    bonus: "Welcome Bonus ₹400",
+    rating: "4.8",
+    downloads: "7M+",
+    trusted: true,
+    link: "games/spin101.html"
+  },
+      {
+    name: "Spin777",
+    image: "spin777.png",
+    bonus: "Welcome Bonus ₹450",
+    rating: "4.8",
+    downloads: "6M+",
+    trusted: true,
+    link: "games/spin777.html"
+  },
+  {
+    name: "Spin Winner",
+    image: "spinwinner.png",
+    bonus: "Welcome Bonus ₹500",
+    rating: "4.9",
+    downloads: "8M+",
+    trusted: true,
+    link: "games/spinwinner.html"
+  },
+  {
+    name: "Safe Earn",
+    image: "safeearn.png",
+    bonus: "Welcome Bonus ₹250",
+    rating: "4.7",
+    downloads: "4M+",
+    trusted: true,
+    link: "games/safeearn.html"
+  },
+  {
+    name: "Slots Spin",
+    image: "slotsspin.png",
+    bonus: "Welcome Bonus ₹350",
+    rating: "4.8",
+    downloads: "5M+",
+    trusted: true,
+    link: "games/slotsspin.html"
+  },
+  {
+    name: "Share477",
+    image: "share477.png",
+    bonus: "Welcome Bonus ₹300",
+    rating: "4.8",
+    downloads: "5M+",
+    trusted: true,
+    link: "games/share477.html"
+  },
+  {
+    name: "Rumble Rummy",
+    image: "rumblerummy.png",
+    bonus: "Welcome Bonus ₹400",
+    rating: "4.8",
+    downloads: "6M+",
+    trusted: true,
+    link: "games/rumblerummy.html"
+  },
+  {
+    name: "OK Rummy",
+    image: "okrummy.png",
+    bonus: "Welcome Bonus ₹350",
+    rating: "4.8",
+    downloads: "5M+",
+    trusted: true,
+    link: "games/okrummy.html"
+  },
+  {
+    name: "Rummy77",
+    image: "rummy77.png",
+    bonus: "Welcome Bonus ₹500",
+    rating: "4.9",
+    downloads: "9M+",
+    trusted: true,
+    link: "games/rummy77.html"
+  },
+  {
+    name: "Rummy888 VIP",
+    image: "rummy888vip.png",
+    bonus: "Welcome Bonus ₹500",
+    rating: "4.9",
+    downloads: "8M+",
+    trusted: true,
+    link: "games/rummy888vip.html"
+  },
+  {
+    name: "Rum Ludo",
+    image: "rumludo.png",
+    bonus: "Welcome Bonus ₹300",
+    rating: "4.8",
+    downloads: "5M+",
+    trusted: true,
+    link: "games/rumludo.html"
+  },
+      {
+    name: "Rummy91",
+    image: "rummy91.png",
+    bonus: "Welcome Bonus ₹400",
+    rating: "4.8",
+    downloads: "6M+",
+    trusted: true,
+    link: "games/rummy91.html"
+  },
+  {
+    name: "Saga Slots",
+    image: "sagaslots.png",
+    bonus: "Welcome Bonus ₹350",
+    rating: "4.8",
+    downloads: "5M+",
+    trusted: true,
+    link: "games/sagaslots.html"
+  },
+  {
+    name: "Teen Patti",
+    image: "teenpatti.png",
+    bonus: "Welcome Bonus ₹300",
+    rating: "4.8",
+    downloads: "9M+",
+    trusted: true,
+    link: "games/teenpatti.html"
+  },
+  {
+    name: "Spin Gold",
+    image: "spingold.png",
+    bonus: "Welcome Bonus ₹400",
+    rating: "4.8",
+    downloads: "6M+",
+    trusted: true,
+    link: "games/spingold.html"
+  },
+  {
+    name: "77 Game",
+    image: "77game.png",
+    bonus: "Welcome Bonus ₹250",
+    rating: "4.7",
+    downloads: "5M+",
+    trusted: true,
+    link: "games/77game.html"
+  },
+  {
+    name: "Good Slots",
+    image: "goodslots.png",
+    bonus: "Welcome Bonus ₹500",
+    rating: "4.9",
+    downloads: "8M+",
+    trusted: true,
+    link: "games/goodslots.html"
+  },
+  {
+    name: "Diwa777",
+    image: "diwaslots.png",
+    bonus: "Welcome Bonus ₹400",
+    rating: "4.8",
+    downloads: "6M+",
+    trusted: true,
+    link: "games/diwa777.html"
+  },
+  {
+    name: "Diwa Slots",
+    image: "diwaslots.png",
+    bonus: "Welcome Bonus ₹350",
+    rating: "4.8",
+    downloads: "5M+",
+    trusted: true,
+    link: "games/diwaslots.html"
+  },
+  {
+    name: "Max Rummy",
+    image: "gamerummy.png",
+    bonus: "Welcome Bonus ₹500",
+    rating: "4.9",
+    downloads: "10M+",
+    trusted: true,
+    link: "games/maxrummy.html"
+  },
+  {
+    name: "All YONO Games New",
+    image: "allyonogamesnew.png",
+    bonus: "Latest Games Collection",
+    rating: "5.0",
+    downloads: "50M+",
+    trusted: true,
+    link: "index.html"
+  }
 ];
 
 const gameList = document.getElementById("gameList");
+const searchInput = document.getElementById("searchInput");
 
-function displayGames(list){
+function renderGames(list){
+  gameList.innerHTML = "";
 
-    gameList.innerHTML = "";
+  list.forEach(game => {
+    const card = document.createElement("div");
+    card.className = "game-card";
 
-    list.forEach((game,index)=>{
+    card.innerHTML = `
+      <img src="images/${game.image}" alt="${game.name}">
+      <h3>${game.name}</h3>
 
-        const card = document.createElement("div");
+      <div class="meta">
+        <span>⭐ ${game.rating}</span>
+        <span>📥 ${game.downloads}</span>
+      </div>
 
-        card.className = "game-card";
+      <p class="bonus">${game.bonus}</p>
 
-        card.innerHTML = `
+      ${game.trusted ? '<div class="trusted">✔ Trusted</div>' : ''}
 
-            <img src="images/${game.image}" alt="${game.name}">
+      <a href="${game.link}" class="download-btn">
+        Download
+      </a>
+    `;
 
-            <h3>${game.name}</h3>
-
-            <div class="rating">
-                ⭐⭐⭐⭐⭐ ${game.rating}
-            </div>
-
-            <div class="bonus">
-                🎁 ${game.bonus}
-            </div>
-
-            <a href="${game.link}"
-               target="_blank"
-               rel="nofollow sponsored noopener"
-               class="download-btn">
-
-               Download
-
-            </a>
-
-        `;
-
-        gameList.appendChild(card);
-
-    });
-
+    gameList.appendChild(card);
+  });
 }
 
-displayGames(games);
+renderGames(games);
 
-document.getElementById("searchInput").addEventListener("input",function(){
+searchInput.addEventListener("input", function(){
+  const value = this.value.toLowerCase();
 
-    const search = this.value.toLowerCase();
+  const filtered = games.filter(game =>
+    game.name.toLowerCase().includes(value)
+  );
 
-    const filtered = games.filter(game =>
-        game.name.toLowerCase().includes(search)
-    );
-
-    displayGames(filtered);
-
+  renderGames(filtered);
 });
